6a390d1aac36e429eeba591a2e6565e8da33a43d
koflin <colin.pfingstl@hotmail.ch>
2018-09-07 19:17:23 +0200

    07.09.2018 [STABLE]
    -added menu
    -added camera movement
    -added character movement
    -added character animations

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public float scrollingSpeed = 0.5f;
+
+    public Transform target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    void FixedUpdate()
+    {
+        Camera camera = GetComponent<Camera>();
+        float sizeChange = Input.mouseScrollDelta.y * scrollingSpeed;
+
+        if (camera.orthographicSize - sizeChange > 0 && camera.orthographicSize - sizeChange < 32)
+        {
+            GetComponent<Camera>().orthographicSize -= sizeChange;
+        }
+    }
+
+    void LateUpdate()
+    {
+        this.transform.SetPositionAndRotation(new Vector3(target.position.x, target.position.y, transform.position.z), transform.rotation);
+    }
+}
---
+++ Assets/Scripts/CharController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class CharController : MonoBehaviour {
+
+    CharacterController charController;
+    Animator animator;
+
+    public float forwardSpeed = 1f;
+    public float turnSpeed = 0.1f;
+    public Tilemap tilemap;
+    public TileBase debugTile;
+
+    bool crashed = false;
+
+	// Use this for initialization
+	void Start () {
+        charController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //tilemap.SetTile(new Vector3Int((int)Mathf.Floor(transform.position.x), (int)Mathf.Floor(transform.position.y), 0), debugTile);
+        TileBase tileBase = tilemap.GetTile(new Vector3Int((int)Mathf.Floor(transform.position.x), (int)Mathf.Floor(transform.position.y), 0));
+
+        if (tileBase == null || tileBase.name != "Grey Stones")
+        {
+            crashed = true;
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        if (!crashed)
+        {
+            transform.Rotate(0, 0, -1 * Input.GetAxis("Horizontal") * turnSpeed);
+            charController.Move(transform.TransformDirection(new Vector3(0, forwardSpeed, 0)));
+
+            animator.SetBool("running", true);
+        }
+        else
+        {
+            animator.SetBool("running", false);
+        }
+    }
+}
